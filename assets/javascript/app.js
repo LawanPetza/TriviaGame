e9faacd48151a5902599b1408c522338aba5c542
Lawan Petza <Lawan.Petza@gmail.com>
2018-06-11 00:18:03 -0400

    added index.html

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    //timer
+    var timerNumber = 30;
+
+    //score
+    var numberCorrect = 0;
+    var numberIncorrect = 0;
+    var numberAnswered = 0;
+    var numberUnanswered = 0;
+
+    //aquestion and answer variables
+
+    var answers = [];
+    var currentQuestion = 0;
+
+    var trivia = [
+        
+    ]
+
+})
